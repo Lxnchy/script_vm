596b580ac468a6adef7753caf453b1328b3d43f1
Lxnchy <103829619+Lxnchy@users.noreply.github.com>
2024-09-25 23:27:43 +0200

    Update script_vm.sh

--- script_vm.sh
+++ script_vm.sh
@@ -52,25 +52,21 @@ echo "Pare-feu configuré."
 
 echo "Installation des paquets nécessaires pour Docker..."
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
-echo "Paquets pour Docker installés."
 
 echo "Ajout de la clé GPG Docker..."
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "Clé GPG Docker ajoutée."
+echo "Clé GPG ajoutée."
 
 echo "Ajout du dépôt Docker à APT sources..."
-echo "deb [arch=\$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-echo "Dépôt Docker ajouté."
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo "Mise à jour des paquets pour Docker..."
+echo "Mise à jour des paquets avec le dépôt Docker..."
 sudo apt update -y
-echo "Mise à jour des paquets terminée."
 
 echo "Installation de Docker..."
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-echo "Docker installé."
 
-echo "Vérification du statut de Docker..."
+echo "Vérification de l'installation et du statut de Docker..."
 if systemctl status docker | grep -q "active (running)"; then
     echo "Docker est correctement installé et fonctionne."
 else
@@ -78,27 +74,21 @@ else
     exit 1
 fi
 
-echo "Vérification si le groupe docker existe..."
+echo "Création du groupe docker s'il n'existe pas..."
 if ! getent group docker > /dev/null 2>&1; then
-    echo "Le groupe docker n'existe pas, création du groupe docker..."
     sudo groupadd docker
     echo "Groupe docker créé."
-else
-    echo "Le groupe docker existe déjà."
 fi
 
-echo "Ajout de l'utilisateur actuel au groupe docker..."
+echo "Ajout de l'utilisateur actuel au groupe docker pour éviter d'utiliser sudo..."
 sudo usermod -aG docker $USER
-echo "Utilisateur ajouté au groupe docker."
 
 echo "Activation de Docker au démarrage..."
 sudo systemctl enable docker
-echo "Docker activé au démarrage."
 
 echo "Installation de Docker Compose..."
 VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
 sudo curl -L "https://github.com/docker/compose/releases/download/$VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-echo "Docker Compose installé."
 
-echo "Installation terminée. Veuillez redémarrer votre session pour que les modifications du groupe docker prennent effet."
+echo "Installation terminée. Veuillez redémarrer votre session pour appliquer les modifications du groupe docker."
